5d26cc5ef3bc45a7f1d6fdb42ada20bde3fb61ce
Walid Tolba <wrt0624@gmail.com>
2025-01-29 09:13:39 +0100

    Added Empty list message

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,14 @@
 function ListGroup() {
+  const items = ["Meskiana", "Oum El Bouaghi", "Alger", "Oran", "Annaba"];
+
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item here!</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
