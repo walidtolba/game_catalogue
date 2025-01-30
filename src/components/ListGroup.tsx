e5a7661d7b3fa9010eef4d6a20737317a3b7717a
Walid Tolba <wrt0624@gmail.com>
2025-01-30 10:04:30 +0100

    fixed bugs

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = ["Meskiana", "Oum El Bouaghi", "Alger", "Oran", "Annaba"];
+interface ListGroupProps {
+    items: string[];
+    heading: string;
+}
+
+function ListGroup({items, heading}: ListGroupProps) {
   const [selectedItem, setSelectedItem] = useState(-1);
 
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item here!</p>}
       <ul className="list-group">
         {items.map((item, index) => (
